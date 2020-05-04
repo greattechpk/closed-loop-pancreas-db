1ea1049a53d2fa193c0cc9ff80168d261163d4cd
greattechpk <great.techpk@gmail.com>
2020-05-04 13:21:35 -0400

    added fixed correction

--- public/js/calc.js
+++ public/js/calc.js
@@ -2,12 +2,23 @@ console.log("connected with calc.js")
 //retrieve inputs for correction
 const formBG = document.getElementById('formBG')
 const formTCorrect = document.getElementById('formTCorrect')
+const formFCorrect = document.getElementById('formFCorrect')
 //retrieve passed data
 const valCorrectSub = Number(document.getElementById('valCorrectSub').textContent)
 const valCorrectDiv = Number(document.getElementById('valCorrectDiv').textContent)
 const valActive = Number(document.getElementById('valActive').textContent)
 
+
+
 formBG.addEventListener("keyup", ()=>{
-    console.log(valCorrectSub)
+    console.log(valActive)
     formTCorrect.value = (formBG.value - valCorrectSub) / valCorrectDiv
+    formFCorrect.value = formTCorrect.value - valActive
+    if(formTCorrect.value < 0){
+        formTCorrect.value = 0
+    }
+    if(formFCorrect.value < 0){
+        formFCorrect.value = 0
+    }
+
 })
